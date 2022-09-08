f46e041ddcddbb02a200605eb4ee0535c6313c47
rkdnd <gaweon.kim@dream-academy.io>
2022-09-08 14:59:40 +0900

    pyramid

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
 contract Pyramid {
-    function run(uint size) pure public returns (string memory _out) {
+    function run(uint size) view public returns (string memory _out) {
         _out = ""; // TODO
-        /* Example (size = 5):
-         *
-         *     "*\n"
-         *     "**\n"
-         *     "***\n"
-         *     "****\n"
-         *     "*****\n"
-         *
-         */
+
+        for (uint i=0; i<size; i++){
+            for (uint k=0; k<i+1; k++)
+                _out = string.concat(_out, "*");
+            _out = string.concat(_out, "\n");
+        }
+
+        console.log(_out);
     }
 }
